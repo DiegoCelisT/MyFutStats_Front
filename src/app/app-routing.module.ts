658a8ebba74d0e54b8d941b5307dac341300e7ea
Diego Celis <diegofercelis@gmail.com>
2021-04-28 02:52:45 -0300

    criadas as primeiras rotas e as conexoes entre elas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { ClubesComponent } from './componentes/clubes/clubes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'clubes', component:ClubesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
