ee75ddd21d1c26a5a91d1adee16f065180c5f307
Diego Celis <diegofercelis@gmail.com>
2021-04-28 04:53:22 -0300

    Mostrando todos os dados da BD de maneira formatada no template, comecando a implementar o metodo POST

--- src/app/componentes/clubes/clubes.component.ts
+++ src/app/componentes/clubes/clubes.component.ts
@@ -34,6 +34,12 @@ export class ClubesComponent implements OnInit {
       this.resultados = resultados ['clubes'] //'É importante declarar o nome do JSON que foi dado no back-end, para dessa maneira evitar o erro de cannot read property '0' of undefined
       console.log (resultados)
     })
+
+    //METODO POST PARA CRIAR NOVOS REGISTROS DESDE O FRONT (comentado para que não crie automaticamente um novo registro cada vez que a gente recarrega a página)
+    // this.FutebolServ.createClube ("Santa Fé", "https://upload.wikimedia.org/wikipedia/commons/5/58/Escudo_de_Independiente_Santa_Fe.png", "Colômbia", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    // .subscribe ();
+
+
   }
 
 }
--- src/app/services/futebol.service.ts
+++ src/app/services/futebol.service.ts
@@ -16,4 +16,25 @@ export class FutebolService {
     return this.httpFutebol.get ('http://localhost:'+this.port+'/clubes')
   }
 
+  //Método POST, para criar novos registros: (para este verbo é necessario passar os parametros no corpo)
+  // createClube (name: string, urlShield: string, country: string, position: number, pts: number, J: number, V: number, E: number, D: number, GP: number, GC: number, SG: number, YC: number, RC: number){
+  //   return this.httpFutebol.post ('http://localhost:'+this.port+'/clubes', {
+  //             name: name,
+  //             urlShield: urlShield,
+  //             country: country,
+  //             position: position,
+  //             pts: pts,
+  //             J: J,
+  //             V: V,
+  //             E: E,
+  //             D: D,
+  //             GP: GP,
+  //             GC: GC,
+  //             SG: SG,
+  //             YC: YC,
+  //             RC: RC
+  //             })
+  //   }
+
+
 }
