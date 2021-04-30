6ff548b9e583f04733842a28c60022bef2a50138
julián Boetto <malostratoscba@gmail.com>
2021-04-29 22:27:41 -0300

    Começo metodo PUT para editar o clube

--- src/app/componentes/addclube/addclube.component.ts
+++ src/app/componentes/addclube/addclube.component.ts
@@ -10,11 +10,12 @@ import { FutebolService } from '../../services/futebol.service';
 
 export class AddclubeComponent implements OnInit {
 
-  //DADOS DO FORMULARIO ADDCLUBE
+  // DADOS DO FORMULARIO ADDCLUBE
   name: string;
   country: string;
   urlShield: string;
 
+  //ARRAY PARA ALMACENAR OS DADOS
   dadosClube = [];
 
   constructor(private FutebolServ: FutebolService) {
@@ -25,7 +26,7 @@ export class AddclubeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  //METODO POST PARA CRIAR NOVOS REGISTROS DESDE O FRONT (PRECISAMOS PEGAR DADOS DO FORMULARIO)   "Santa Fé", "https://upload.wikimedia.org/wikipedia/commons/5/58/Escudo_de_Independiente_Santa_Fe.png", "Colômbia", 
+  //METODO POST PARA CRIAR NOVOS REGISTROS DESDE O FRONT (PRECISAMOS PEGAR DADOS DO FORMULARIO) DEJO LOS DATOS DE TU CLUB KKKK "Santa Fé", "https://upload.wikimedia.org/wikipedia/commons/5/58/Escudo_de_Independiente_Santa_Fe.png", "Colômbia", 
   novoClub(){
     console.log('novoClub')
     this.FutebolServ.createClube(this.name, this.urlShield, this.country, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
@@ -34,23 +35,24 @@ export class AddclubeComponent implements OnInit {
   }
   
   //BUSCA DADOS DO FORMULARIO E CRIA UM ARRAY PARA USAR NO FUTURO
-  buscar(dadosClube) {
+  buscar(dadosClube: any) {
     // location.href = `/resultados/${form.value.pesquisa}`;
-    dadosClube.push(this.name);
-    dadosClube.push(this.country);
-    dadosClube.push(this.urlShield);
-    dadosClube.push(0);
-    dadosClube.push(0);
-    dadosClube.push(0);
-    dadosClube.push(0);
-    dadosClube.push(0);
-    dadosClube.push(0);
-    dadosClube.push(0);
-    dadosClube.push(0);
-    dadosClube.push(0);
-    dadosClube.push(0);
-    dadosClube.push(0); 
-    console.log(dadosClube)
+    var dadosJson = {
+      name: this.name,
+      urlShield: this.urlShield,
+      country: this.country,
+      position: 0,
+      pts: 0,
+      J: 0,
+      V: 0,
+      E: 0,
+      D: 0,
+      GP: 0,
+      GC: 0,
+      SG: 0,
+      YC: 0,
+      RC: 0}
+    dadosClube.push(dadosJson);
     this.novoClub();
   }
 
