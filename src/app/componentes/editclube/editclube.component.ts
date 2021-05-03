209779463480eccf86b05c2ea175fb2563c41295
julián Boetto <malostratoscba@gmail.com>
2021-05-03 18:28:16 -0300

    Solucionado erro METODO PUT, melhorias validação

--- src/app/componentes/editclube/editclube.component.ts
+++ src/app/componentes/editclube/editclube.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FutebolService } from '../../services/futebol.service';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-editclube',
   templateUrl: './editclube.component.html',
@@ -10,12 +11,11 @@ import { ActivatedRoute } from '@angular/router';
 export class EditclubeComponent implements OnInit {
 
   resultado: any = [];
-
+   
   ID: any;
   idNum: Number;
 
   // PARA EDITAR CLUB
-    
   name: string;
   urlShield: string;
   country: string;
@@ -31,8 +31,7 @@ export class EditclubeComponent implements OnInit {
   YC: number;
   RC: number;
 
-  //ARRAY PARA ALMACENAR OS DADOS
-  dadosEditClube = [];
+  
 
   constructor(private FutebolServ: FutebolService, private roteClub: ActivatedRoute) { }
 
@@ -46,38 +45,19 @@ export class EditclubeComponent implements OnInit {
               this.resultado = resultados ['clube'] 
             })  
         });
+        console.log(this.ID)
+    // this.validate();
   }
 
   //METODO PUT
   editClub(){
-    this.FutebolServ.editClube (this.ID, this.name, this.urlShield, this.country, this.position, this.pts, this.J, this.V, this.E, this.D, this.GP, this.GC, this.SG, this.YC, this.RC)
-    .subscribe();
-    console.log('clubatualizado')
+    this.FutebolServ.editClube(this.ID, this.name, this.urlShield, this.country, this.position, this.pts, this.J, this.V, this.E, this.D, this.GP, this.GC, this.SG, this.YC, this.RC)
+    .subscribe() 
+    console.log('editclub component'+ this.ID, this.name, this.urlShield, this.country, this.position, this.pts, this.J, this.V, this.E, this.D, this.GP, this.GC, this.SG, this.YC, this.RC )
     alert('CLUBE ATUALIZADO') //ES SOLO PARA DAR AVISO
   }
 
-  
-  //BUSCA DADOS DO FORMULARIO E CRIA UM ARRAY PARA USAR NO FUTURO
-  buscar(dadosEditClube: any) {
-  var dadosJson = {
-    name: this.name,
-    urlShield: this.urlShield,
-    country: this.country,
-    position: this.position,
-    pts: this.pts,
-    J: this.J,
-    V: this.V,
-    E: this.E,
-    D: this.D,
-    GP: this.GP,
-    GC: this.GC,
-    SG: this.SG,
-    YC: this.YC,
-    RC: this.RC}
-  dadosEditClube.push(dadosJson);
-  console.log('buscar()' + this.J,this.E)
-  this.editClub();
-  }
+
 
   dadosRandom() {
     this.FutebolServ.sumarJ(); //SUMA UN PARTIDO CADA VEZ QUE EL BOTON ES APRETADO
@@ -90,6 +70,14 @@ export class EditclubeComponent implements OnInit {
     this.GP = 10;
     this.GC = 10;
   }
+
+  // validate(){
+  //   var form = document.getElementsByClassName('needs-validation')[0] as HTMLFormElement
+  //   form.classList.add('was-validated');
+  // }
+
+  
+
  
 }
 
