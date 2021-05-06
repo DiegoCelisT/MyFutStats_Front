e497ad3bc887ff2ff897a4215c5db9a0d4a7b6c3
Diego Celis <diegofercelis@gmail.com>
2021-05-06 19:09:27 -0300

    correcao de bugs e melhoria no componente editClube

--- src/app/componentes/editclube/editclube.component.ts
+++ src/app/componentes/editclube/editclube.component.ts
@@ -24,17 +24,25 @@ export class EditclubeComponent implements OnInit {
   derrotas: number;
   golsPro: number;
   golsContra: number;
-
+  
   formularioEdit: FormGroup;
+
+  // pontos:number;
+  // jogados:number;
+  // saldoGols:number
 
   nomeLigas = [{
     name:String
   }];
-
-   // BOTÃO EDITAR DESHABILITADO
-   botonEdit = false;
   
-  constructor(private FutebolServ: FutebolService, private roteClub: ActivatedRoute, private formEdit: FormBuilder) { }
+  // BOTÃO EDITAR DESHABILITADO
+  botonEdit = false;
+
+  constructor(private FutebolServ: FutebolService, private roteClub: ActivatedRoute
+    // , private formValidacion: FormBuilder
+    ) {
+    
+  }
 
   ngOnInit(): void {
         //USA EL ID DE LA URL PARA MOSTRAR UN CLUB
@@ -53,16 +61,16 @@ export class EditclubeComponent implements OnInit {
     })
     this.validEdit();
 
-    this.formularioEdit = this.formEdit.group({
-      name: [null],
-      country: [null],
-      // urlShield: [null],
-      vitorias: [null],
-      empates: [null],
-      derrotas: [null],
-      golsPro: [null],
-      golsContra: [null],
-    })
+    // this.formularioEdit = this.formEdit.group({
+    //   name: [null],
+    //   country: [null],
+    //   urlShield: [null],
+    //   vitorias: [null],
+    //   empates: [null],
+    //   derrotas: [null],
+    //   golsPro: [null],
+    //   golsContra: [null],
+    // })
 
   }
 
@@ -74,6 +82,7 @@ export class EditclubeComponent implements OnInit {
     if (this.derrotas==null){ this.derrotas=this.resultado.derrotas }
     if (this.golsPro==null){ this.golsPro=this.resultado.golsPro }
     if (this.golsContra==null){ this.golsContra=this.resultado.golsContra }
+    if (this.urlShield==""){ this.urlShield="https://www.clipartmax.com/png/full/19-194040_how-to-set-use-shield-grey-svg-vector-shield-template.png" }
 
     this.FutebolServ.editClube(this.ID, this.name, this.urlShield, this.country, this.vitorias, this.empates, this.derrotas, this.golsPro, this.golsContra)
       .subscribe()
@@ -96,10 +105,5 @@ export class EditclubeComponent implements OnInit {
     var form = document.getElementsByClassName('needs-validation')[0] as HTMLFormElement
     form.classList.add('was-validated');
   }
-
-  
-  
  
 }
-
-
