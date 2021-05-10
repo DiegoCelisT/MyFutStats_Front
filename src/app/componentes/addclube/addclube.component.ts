ffbf76148e9e5cf6572ee79d337c717895aa8702
Diego Celis <diegofercelis@gmail.com>
2021-05-10 06:43:54 -0300

    refatorizacao para testes das novas tabelas usando um parametro unico

--- src/app/componentes/addclube/addclube.component.ts
+++ src/app/componentes/addclube/addclube.component.ts
@@ -55,7 +55,6 @@ export class AddclubeComponent implements OnInit {
     if (this.formularioAdd.value.golsPro==null){ golsPro=0 }
     if (this.formularioAdd.value.golsContra==null){ golsContra=0 }
     if (urlShield==null){ urlShield="https://www.clipartmax.com/png/full/19-194040_how-to-set-use-shield-grey-svg-vector-shield-template.png" }
-
     this.FutebolServ.createClube(name, urlShield, country, vitorias, empates, derrotas, golsPro, golsContra)
     .subscribe()
   }
