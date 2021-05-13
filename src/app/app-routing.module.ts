e017bab1f31f2ac101b6fb55c96c88491c78c452
Diego Celis <diegofercelis@gmail.com>
2021-05-13 06:16:15 -0300

    retirando componentes iterados e deixando só os componentes automatizados para todas as ligas e clubes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,6 @@ import { ClubeComponent } from './componentes/liga1/clube/clube.component';
 import { AddclubeComponent } from './componentes/liga1/addclube/addclube.component';
 import { EditligaComponent } from './componentes/liga1/editliga/editliga.component';
 
-import { Editclube2Component } from './componentes/liga2/editclube/editclube2.component';
-
-import { Editclube3Component } from './componentes/liga3/editclube/editclube3.component';
-
-import { Editclube4Component } from './componentes/liga4/editclube/editclube4.component'
-
-import { Editclube5Component } from './componentes/liga5/editclube/editclube5.component'
-
-import { Editclube6Component } from './componentes/liga6/editclube/editclube6.component'
-
 const routes: Routes = [
   { path: '', component:HomeComponent },
   { path: 'liga/:idLiga/clube/:id', component:ClubeComponent },
@@ -26,16 +16,6 @@ const routes: Routes = [
   { path: 'liga/:idLiga/addclube', component:AddclubeComponent },
   { path: 'liga/:idLiga/editclube/:id', component:EditclubeComponent },
   { path: 'liga/:idLiga/editliga', component:EditligaComponent },
-
-  { path: 'liga2/editclube/:id', component:Editclube2Component },
-
-  { path: 'liga3/editclube/:id', component:Editclube3Component },
-
-  { path: 'liga4/editclube/:id', component:Editclube4Component },
-
-  { path: 'liga5/editclube/:id', component:Editclube5Component },
-
-  { path: 'liga6/editclube/:id', component:Editclube6Component },
 ];
 
 @NgModule({
--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -10,36 +10,6 @@ import { ClubeComponent } from './liga1/clube/clube.component';
 import { AddclubeComponent } from './liga1/addclube/addclube.component';
 import { EditligaComponent } from './liga1/editliga/editliga.component';
 
-import { Liga2Component } from './liga2/liga/liga2.component';
-import { Editclube2Component } from './liga2/editclube/editclube2.component'
-import { Clube2Component } from './liga2/clube/clube2.component';
-import { Addclube2Component } from './liga2/addclube/addclube2.component';
-import { Editliga2Component } from './liga2/editliga/editliga2.component';
-
-import { Liga3Component } from './liga3/liga/liga3.component';
-import { Editclube3Component } from './liga3/editclube/editclube3.component'
-import { Clube3Component } from './liga3/clube/clube3.component';
-import { Addclube3Component } from './liga3/addclube/addclube3.component';
-import { Editliga3Component } from './liga3/editliga/editliga3.component';
-
-import { Liga4Component } from './liga4/liga/liga4.component';
-import { Editclube4Component } from './liga4/editclube/editclube4.component'
-import { Clube4Component } from './liga4/clube/clube4.component';
-import { Addclube4Component } from './liga4/addclube/addclube4.component';
-import { Editliga4Component } from './liga4/editliga/editliga4.component';
-
-import { Liga5Component } from './liga5/liga/liga5.component';
-import { Editclube5Component } from './liga5/editclube/editclube5.component'
-import { Clube5Component } from './liga5/clube/clube5.component';
-import { Addclube5Component } from './liga5/addclube/addclube5.component';
-import { Editliga5Component } from './liga5/editliga/editliga5.component';
-
-import { Liga6Component } from './liga6/liga/liga6.component';
-import { Editclube6Component } from './liga6/editclube/editclube6.component'
-import { Clube6Component } from './liga6/clube/clube6.component';
-import { Addclube6Component } from './liga6/addclube/addclube6.component';
-import { Editliga6Component } from './liga6/editliga/editliga6.component';
-
 //Módulos para pesquisar e para ordenar:
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { OrderModule } from 'ngx-order-pipe';
@@ -52,36 +22,6 @@ import { OrderModule } from 'ngx-order-pipe';
     ClubeComponent,
     AddclubeComponent,
     EditligaComponent,
-
-    Liga2Component,
-    Editclube2Component,
-    Clube2Component,
-    Addclube2Component,
-    Editliga2Component,
-
-    Liga3Component,
-    Editclube3Component,
-    Clube3Component,
-    Addclube3Component,
-    Editliga3Component,
-
-    Liga4Component,
-    Editclube4Component,
-    Clube4Component,
-    Addclube4Component,
-    Editliga4Component,
-
-    Liga5Component,
-    Editclube5Component,
-    Clube5Component,
-    Addclube5Component,
-    Editliga5Component,
-
-    Liga6Component,
-    Editclube6Component,
-    Clube6Component,
-    Addclube6Component,
-    Editliga6Component,
   ],
   imports: [
     CommonModule,
@@ -98,36 +38,6 @@ import { OrderModule } from 'ngx-order-pipe';
     ClubeComponent,
     AddclubeComponent,
     EditligaComponent,
-
-    Liga2Component,
-    Editclube2Component,
-    Clube2Component,
-    Addclube2Component,
-    Editliga2Component,
-
-    Liga3Component,
-    Editclube3Component,
-    Clube3Component,
-    Addclube3Component,
-    Editliga3Component,
-
-    Liga4Component,
-    Editclube4Component,
-    Clube4Component,
-    Addclube4Component,
-    Editliga4Component,
-
-    Liga5Component,
-    Editclube5Component,
-    Clube5Component,
-    Addclube5Component,
-    Editliga5Component,
-
-    Liga6Component,
-    Editclube6Component,
-    Clube6Component,
-    Addclube6Component,
-    Editliga6Component,
   ]
 })
 export class ComponentesModule { }
